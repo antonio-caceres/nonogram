a3a3e086e66c1de78255fd7f447c525163f9b95f
Antonio Caceres <antonio@antonio-caceres.com>
2025-02-10 13:39:45 -0800

    Combine satisfied tests into an application of product.
    Update constants with name accordingly.

--- tests/test_nonoclue.py
+++ tests/test_nonoclue.py
@@ -4,25 +4,18 @@ from unittest import TestCase
 from nonogram.core import Nonoclue
 
 
-class EmptyNonoclue(TestCase):
-    _NUM_CASES_ISH = 10  # number of test cases plus-minus one
+_CASES_LINISH = 5  # number of test cases plus-minus one
+_CASES_EXPISH = 5  # if number of cases grow exponentially
 
+
+class EmptyNonoclue(TestCase):
     def test_initialization(self):
-        for clue in [[0] * i for i in range(3)]:
+        for clue in [[0] * i for i in range(_CASES_LINISH)]:
             with self.subTest(init_arg=clue):
                 self.assertEqual(Nonoclue(clue).clue, [])
 
-    def test_satisfied_true(self):
-        for sol in [[0] * i for i in range(self._NUM_CASES_ISH)]:
+    def test_satisfied(self):
+        for sol in itertools.product((False, True), repeat=_CASES_EXPISH):
             with self.subTest(sequence=sol):
-                self.assertTrue(Nonoclue([]).satisfied_by(sol))
-
-    def test_satisfied_false(self):
-        sols = [[1] * i for i in range(1, self._NUM_CASES_ISH)]
-        for i in range(self._NUM_CASES_ISH):
-            base = [0] * self._NUM_CASES_ISH
-            base[i] = 1
-            sols.append(base)
-        for sol in sols:
-            with self.subTest(sequence=sol):
-                self.assertFalse(Nonoclue([]).satisfied_by(sol))
+                self.assertEqual(Nonoclue([]).satisfied_by(sol),
+                                 not any(sol))
