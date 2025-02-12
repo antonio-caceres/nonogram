4a6f0b0c82713f5a6bc77b04fb47ced4fc433e70
Antonio Caceres <antonio@antonio-caceres.com>
2025-02-12 13:00:17 -0800

    Add test for data input truncating and padding.

--- tests/test_nonogrid.py
+++ tests/test_nonogrid.py
@@ -1,3 +1,4 @@
+import itertools
 from unittest import TestCase
 
 from nonogram.core import Nonogrid
@@ -10,6 +11,15 @@ class BasicPropertyInitialization(TestCase):
         self.assertEqual(grid.dims, (exp_height, exp_width))
         self.assertEqual(grid.height, exp_height)
         self.assertEqual(grid.width, exp_width)
+
+    def test_data_input_dims(self):
+        max_side = 2 * 2
+        data = [[None] * (max_side // 2)] * (max_side // 2)
+
+        for exp_height, exp_width in itertools.product(range(max_side), range(max_side)):
+            with self.subTest(height=exp_height, width=exp_width):
+                grid = Nonogrid(exp_height, exp_width, data)
+                self.assertEqual(grid.dims, (exp_height, exp_width))
 
     def test_map_default(self):
         grid = Nonogrid(0, 0)
