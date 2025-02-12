a5bfc3eea649300b353dcfe6e6505b02bc627a34
Antonio Caceres <antonio@antonio-caceres.com>
2025-02-12 12:59:36 -0800

    Rename nonogrid test variables.

--- tests/test_nonogrid.py
+++ tests/test_nonogrid.py
@@ -5,17 +5,17 @@ from nonogram.core import Nonogrid
 
 class BasicPropertyInitialization(TestCase):
     def test_dims(self):
-        arg_height, arg_width = 1, 2
-        result = Nonogrid(arg_height, arg_width)
-        self.assertEqual(result.dims, (arg_height, arg_width))
-        self.assertEqual(result.height, arg_height)
-        self.assertEqual(result.width, arg_width)
+        exp_height, exp_width = 1, 2
+        grid = Nonogrid(exp_height, exp_width)
+        self.assertEqual(grid.dims, (exp_height, exp_width))
+        self.assertEqual(grid.height, exp_height)
+        self.assertEqual(grid.width, exp_width)
 
     def test_map_default(self):
-        result = Nonogrid(0, 0)
-        self.assertEqual(result.bool_map, bool)
+        grid = Nonogrid(0, 0)
+        self.assertEqual(grid.bool_map, bool)
 
     def test_custom_map(self):
         f = lambda: True
-        result = Nonogrid(0, 0, bool_map=f)
-        self.assertEqual(result.bool_map, f)
+        grid = Nonogrid(0, 0, bool_map=f)
+        self.assertEqual(grid.bool_map, f)
