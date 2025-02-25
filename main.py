e51ce4cc51ab277bb6c1d93ee486b15ac1cad5e4
Antonio Caceres <antonio@antonio-caceres.com>
2025-02-25 13:37:12 -0800

    Adapt main.py to handle unsolvable nonogrids.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from nonogram import Nonogram
+from nonogram.solve.abc import SolveFailure
 from nonogram.solve.detsearch import NaiveSolver
 
 from datascript import rule_input
@@ -15,9 +16,15 @@ print(f"\n{progress_msg}", end="")
 solver = NaiveSolver(Nonogram(clues["row"], clues["col"]))
 grid = solver.solve()
 
-title = "Solution"
-diff = " " * max(0, len(progress_msg) - len(title))
-print(f"\r{title}{diff}\n{"-" * len(title)}")
+match grid:
+    case SolveFailure.INC:
+        print("\rNo solution found.")
+    case SolveFailure.DNE:
+        print("\rNo solution exists.")
+    case Nonogrid:
+        title = "Solution"
+        diff = " " * max(0, len(progress_msg) - len(title))
+        print(f"\r{title}{diff}\n{"-" * len(title)}")
 
-for row in grid.rows():
-    print("".join("■" if x else "□" for x in row))
+        for row in grid.rows():
+            print("".join("■" if x else "□" for x in row))
