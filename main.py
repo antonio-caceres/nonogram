56d85180b55a982801428a35af180688cffeb7e4
Antonio Caceres <antonio@antonio-caceres.com>
2025-02-26 13:09:30 -0800

    Update main.py to use the new solver.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 
 from nonogram import Nonogram
 from nonogram.solve.abc import SolveFailure
-from nonogram.solve.detsearch import NaiveSolver
+from nonogram.solve import ClueChooser
 
 from datascript import rule_input
 
+max_dim, warning_dim = 16, 8
+
 clues = rule_input()["clues"]
-if len(clues["row"]) > 4 or len(clues["col"]) > 4:
-    print("Greater than 4x4 not supported.")
+if len(clues["row"]) > max_dim or len(clues["col"]) > max_dim:
+    print(f"Larger than {max_dim}x{max_dim} not supported.")
     exit()
+if len(clues["row"]) > warning_dim or len(clues["col"]) > warning_dim:
+    print(f"Nonograms larger than {warning_dim}x{warning_dim} nonograms "
+          f"can take more than 5 minutes to solve.")
+    if input("'Enter' to proceed; type anything to cancel."):
+        print("Solve cancelled.")
+        exit()
 
 progress_msg = "Solving..."
 print(f"\n{progress_msg}", end="")
 
-solver = NaiveSolver(Nonogram(clues["row"], clues["col"]))
+solver = ClueChooser(Nonogram(clues["row"], clues["col"]))
 grid = solver.solve()
 
 match grid:
